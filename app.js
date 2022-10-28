573fe51d976454be59922415148202c58e9367d1
vikyw89 <vikyw89@gmail.com>
2022-10-28 13:38:59 +0800

    4.2 done

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
+logger.info('Connecting to ', mongoUrl)
+
 mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info('Connected to ', mongoUrl)
+    })
+    .catch ((error) => {
+        logger.error('Error connecting to database', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
