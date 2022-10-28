d462c6d1c4be27bb9201607bc90461aaf9335777
vikyw89 <vikyw89@gmail.com>
2022-10-28 12:41:30 +0800

    refactor config refactor logger

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -38,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
