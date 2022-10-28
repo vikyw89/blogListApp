787ef5b89193886c118b4db72be070a3170a48db
vikyw89 <vikyw89@gmail.com>
2022-10-28 15:11:28 +0800

    4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
 }
-  
+
+const totalLikes = (blogs) => {
+    let sum = 0
+    for (let blog of blogs) {
+        sum += blog.likes
+    }
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
